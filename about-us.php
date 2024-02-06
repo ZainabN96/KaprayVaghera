19c42b5d12a8bc2db429ac666113f01e0e8b6e32
Sanashahid <sanashd20@gmail.com>
2024-02-06 15:32:41 +0500

    header,footer included

---
+++ about-us.php
@@ -0,0 +1,204 @@
+<?php include 'include/header.php' ?>
+
+
+		<!-- breadcrumbs area start -->
+		<!-- <div class="breadcrumbs">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="container-inner">
+							<ul>
+								<li class="home">
+									<a href="index.php">Home</a>
+									<span><i class="fa fa-angle-right"></i></span>
+								</li>
+								<li class="category3"><span>About</span></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div> -->
+		<!-- breadcrumbs area end -->
+		<!-- hello about start -->
+		<div class="home-hello-info">
+			<div class="container">
+				<div class="row">
+					<div class="col-12">
+						<div class="f-title text-center">
+							<h3 class="title text-uppercase">About Us</h3>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-7 col-12">
+						<div class="about-page-cntent">
+							<h3>The standard lorem ipsum passage</h3>
+							<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?</p>
+							<blockquote>
+								<p>Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur</p>
+							</blockquote>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eu nisi ac mi malesuada vestibulum. Phasellus tempor nunc eleifend cursus molestie. Mauris lectus arcu, pellentesque at sodales sit amet, condimentum id nunc. Donec ornare mattis suscipit. Praesent fermentum accumsan vulputate.</p>
+						</div>
+					</div>
+					<div class="col-lg-5 col-12">
+						<div class="img-element">
+							<img width="650" height="538" src="img/about/about.webp" alt="banner1">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- hello about end -->
+		<!-- service about start -->
+		<div class="our-services-info">
+			<div class="container">
+				<div class="row">
+					<div class="col-12">
+						<div class="f-title text-center">
+							<h3 class="title text-headss">Our services</h3>
+						</div>
+					</div>
+				</div><!-- End .row -->
+				<div class="row text-center">
+					<div class="our-services-inner">
+						<div class="row">
+							<div class="col-md-4">
+								<div class="single-service">
+									<p><i class="fa fa-star"></i></p>
+									<h4>Branding</h4>
+									<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+								</div><!-- End .single-service -->
+							</div><!-- End .col-md-4 -->
+							<div class="col-md-4">
+								<div class="single-service">
+									<p><i class="fa fa-heart"></i></p>
+									<h4>Awesome Design</h4>
+									<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+								</div><!-- End .single-service -->
+							</div><!-- End .col-md-4 -->
+							<div class="col-md-4">
+								<div class="single-service">
+									<p><i class="fa fa-camera-retro"></i></p>
+									<h4>Photography</h4>
+									<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+								</div><!-- End .single-service -->
+							</div><!-- End .col-md-4 -->
+						</div>
+					</div>
+					<div class="clearfix"></div>
+					<div class="our-services-inner">
+						<div class="row">
+							<div class="col-md-4">
+								<div class="single-service">
+									<p><i class="fa fa-cog"></i></p>
+									<h4>Reponsive</h4>
+									<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+								</div><!-- End .single-service -->
+							</div><!-- End .col-md-4 -->
+							<div class="col-md-4">
+								<div class="single-service">
+									<p><i class="fa fa-eye"></i></p>
+									<h4>Retina ready</h4>
+									<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+								</div><!-- End .single-service -->
+							</div><!-- End .col-md-4 -->
+							<div class="col-md-4">
+								<div class="single-service">
+									<p><i class="fa fa-comments-o"></i></p>
+									<h4>Online Support</h4>
+									<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+								</div><!-- End .single-service -->
+							</div><!-- End .col-md-4 -->
+						</div>
+					</div><!-- End .our-services-inner -->
+				</div><!-- End .row -->
+			</div><!-- End .container -->
+		</div>
+		<!-- service about end -->
+		<!-- meet about start -->
+		<div class="home-our-team">
+			<div class="container">
+				<div class="row">
+					<div class="col-12">
+						<div class="f-title text-center">
+							<h3 class="title text-uppercase">Meet the team</h3>
+						</div>
+					</div><!-- End .col-md-12 -->
+				</div><!-- End .row -->
+				<div class="row">
+					<div class="col-lg-3 col-md-6">
+						<div class="item-team text-center">
+							<div class="team-info">
+								<div class="team-img">
+									<img src="img/about/ab-team1.webp" class="img-responsive" alt="team1" height="600" width="600">
+									<div class="mask">
+										<div class="mask-inner">
+											<a href="#"><i class="fa fa-facebook"></i></a>
+											<a href="#"><i class="fa fa-twitter"></i></a>
+										</div>
+									</div>
+								</div>
+							</div>
+							<h4>Havier Macherano</h4>
+							<h5>Developer</h5>
+						</div><!-- End .item-team -->
+					</div><!-- End .col-sm-3 -->
+					<div class="col-lg-3 col-md-6">
+						<div class="item-team text-center">
+							<div class="team-info">
+								<div class="team-img">
+									<img src="img/about/ab-team2.webp" class="img-responsive" alt="team2" height="601" width="600">
+									<div class="mask">
+										<div class="mask-inner">
+											<a href="#"><i class="fa fa-facebook"></i></a>
+											<a href="#"><i class="fa fa-twitter"></i></a>
+										</div>
+									</div>
+								</div>
+							</div>
+							<h4>Luka Biglia</h4>
+							<h5>Programmer</h5>
+						</div><!-- End .item-team -->
+					</div><!-- End .col-sm-3 -->
+					<div class="col-lg-3 col-md-6">
+						<div class="item-team text-center">
+							<div class="team-info">
+								<div class="team-img">
+									<img src="img/about/ab-team3.webp" class="img-responsive" alt="team3" height="600" width="600">
+									<div class="mask">
+										<div class="mask-inner">
+											<a href="#"><i class="fa fa-facebook"></i></a>
+											<a href="#"><i class="fa fa-twitter"></i></a>
+										</div>
+									</div>
+								</div>
+							</div>
+							<h4>Anzo Perez</h4>
+							<h5>Designer</h5>
+						</div><!-- End .item-team -->
+					</div><!-- End .col-sm-3 -->
+					<div class="col-lg-3 col-md-6">
+						<div class="item-team text-center">
+							<div class="team-info">
+								<div class="team-img">
+									<img src="img/about/ab-team4.webp" class="img-responsive" alt="team4" height="600" width="600">
+									<div class="mask">
+										<div class="mask-inner">
+											<a href="#"><i class="fa fa-facebook"></i></a>
+											<a href="#"><i class="fa fa-twitter"></i></a>
+										</div>
+									</div>
+								</div>
+							</div>
+							<h4>Martin Demichelis</h4>
+							<h5>PHP Developer</h5>
+						</div><!-- End .item-team -->
+					</div><!-- End .col-sm-3 -->
+				</div><!-- End .row -->
+			</div><!-- End .container -->
+		</div>
+		<!-- meet about end -->
+		<!-- FOOTER START -->
+		<?php include 'include/footer.php' ?>
+		
---
+++ include/footer.php
@@ -0,0 +1,259 @@
+	<!-- FOOTER START   -->
+    <footer class="mt-5"> 
+			<!-- banner footer area start -->
+			<div class="banner-footer" style="background-color: black;">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-lg-2 col-md-2 col-3 nopadding">
+							<div class="single-bannerfooter">
+								<a href="#">
+									<img  width="320" height="320" src="img/banner/footer-1.webp" alt="" />
+								</a>
+							</div>
+						</div>
+						<div class="col-lg-2 col-md-2 col-3 nopadding">
+							<div class="single-bannerfooter">
+								<a href="#">
+									<img  width="320" height="320" src="img/banner/footer-2.webp" alt="" />
+								</a>
+							</div>
+						</div>
+						<div class="col-lg-2 col-md-2 col-3 nopadding">
+							<div class="single-bannerfooter">
+								<a href="#">
+									<img  width="320" height="320" src="img/banner/footer-3.webp" alt="" />
+								</a>
+							</div>
+						</div>
+						<div class="col-lg-2 col-md-2 col-3 nopadding">
+							<div class="single-bannerfooter">
+								<a href="#">
+									<img  width="320" height="320" src="img/banner/footer-4.webp" alt="" />
+								</a>
+							</div>
+						</div>
+						<div class="col-lg-2 col-md-2 d-md-block d-none nopadding">
+							<div class="single-bannerfooter">
+								<a href="#">
+									<img  width="320" height="320" src="img/banner/footer-5.webp" alt="" />
+								</a>
+							</div>
+						</div>
+						<div class="col-lg-2 col-md-2 d-md-block d-none nopadding">
+							<div class="single-bannerfooter last-single">
+								<a href="#">
+									<img  width="320" height="320" src="img/banner/footer-6.webp" alt="" />
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- banner footer area end -->
+			<!-- top footer area start -->
+			<div class="top-footer-area">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-3 col-md-4">
+							<div class="single-snap-footer">
+								<div class="snap-footer-title">
+									<h4>Company info</h4>
+								</div>
+								<div class="cakewalk-footer-content">
+									<p class="footer-des">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim adm.</p>
+									<a href="#" class="read-more">Read more</a>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-3 col-md-4">
+							<div class="single-snap-footer">
+								<div class="snap-footer-title">
+									<h4>Information</h4>
+								</div>
+								<div class="cakewalk-footer-content">
+									<ul>
+										<li><a href="about-us.php">About Us</a></li>
+										<li><a href="#">Careers</a></li>
+										<li><a href="#">Delivery Information</a></li>
+										<li><a href="#">Privacy Policy</a></li>
+										<li><a href="#">Terms & Condition</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-2 col-md-4">
+							<div class="single-snap-footer">
+								<div class="snap-footer-title">
+									<h4>Fashion Tags</h4>
+								</div>
+								<div class="cakewalk-footer-content">
+									<ul>
+										<li><a href="#">My Account</a></li>
+										<li><a href="#">Login</a></li>
+										<li><a href="#">My Cart</a></li>
+										<li><a href="#">Wishlist</a></li>
+										<li><a href="#">Checkout</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-2 d-lg-block d-none">
+							<div class="single-snap-footer">
+								<div class="snap-footer-title">
+									<h4>Fashion Tags</h4>
+								</div>
+								<div class="cakewalk-footer-content">
+									<ul>
+										<li><a href="#">Sitemap</a></li>
+										<li><a href="#">Privacy Policy</a></li>
+										<li><a href="#">Advanced Search</a></li>
+										<li><a href="#">Affiliates</a></li>
+										<li><a href="#">Contact Us</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-2 d-lg-block d-none">
+							<div class="single-snap-footer">
+								<div class="snap-footer-title">
+									<h4>Follow Us</h4>
+								</div>
+								<div class="cakewalk-footer-content social-footer">
+									<ul>
+										<li><a href="https://www.facebook.com/" target="_blank"><i class="fa fa-facebook"></i></a><span> Facebook</span></li>
+										<li><a href="https://plus.google.com/" target="_blank"><i class="fa fa-google-plus"></i></a><span> Google Plus</span></li>
+										<li><a href="https://twitter.com/" target="_blank"><i class="fa fa-twitter"></i></a><span> Twitter</span></li>
+										<li><a href="https://youtube.com/" target="_blank"><i class="fa fa-youtube-play"></i></a><span> Youtube</span></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>	
+			<!-- top footer area end -->
+			<!-- info footer start -->
+			<div class="info-footer">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-3 col-md-4">
+							<div class="info-fcontainer">
+								<div class="infof-icon">
+									<i class="fa fa-map-marker"></i>
+								</div>
+								<div class="infof-content">
+									<h3>Our Address</h3>
+									<p>Your address goes here.</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-3 col-md-4">
+							<div class="info-fcontainer">
+								<div class="infof-icon">
+									<i class="fa fa-phone"></i>
+								</div>
+								<div class="infof-content">
+									<h3>Phone Support</h3>
+									<p>0123456789</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-3 col-md-4">
+							<div class="info-fcontainer">
+								<div class="infof-icon">
+									<i class="fa fa-envelope"></i>
+								</div>
+								<div class="infof-content">
+									<h3>Email Support</h3>
+									<p>demo@example.com</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-3 d-lg-block d-none">
+							<div class="info-fcontainer">
+								<div class="infof-icon">
+									<i class="fa fa-clock-o"></i>
+								</div>
+								<div class="infof-content">
+									<h3>Openning Hour</h3>
+									<p>Sat - Thu : 9:00 am - 22:00 pm</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- info footer end -->
+			<!-- footer address area start -->
+			<div class="address-footer">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-6 col-12">
+							<address>2022 &copy; <strong> Kapray Wagara	 </strong> Made with by <a href="https://www.hcc-me.com/hcc-technology-foundation/" target="_blank"><strong>HCC Technology Foundation</strong></a></address>
+						</div>
+						<div class="col-lg-6 col-12">
+							<div class="footer-payment pull-right">
+								<a href="#"><img  width="295" height="25" src="img/payment.webp" alt="" /></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- footer address area end -->
+		</footer>
+		<!-- FOOTER END -->
+		
+        <!-- JS -->
+        
+ 		<!-- jquery-1.11.3.min js
+		============================================ -->         
+        <script src="js/vendor/jquery-1.12.4.min.js"></script>
+        
+ 		<!-- bootstrap js
+		============================================ -->         
+        <script src="js/bootstrap.min.js"></script>
+		
+		<!-- Nivo slider js
+		============================================ --> 		
+		<script src="custom-slider/js/jquery.nivo.slider.js" type="text/javascript"></script>
+		<script src="custom-slider/home.js" type="text/javascript"></script>
+        
+   		<!-- owl.carousel.min js
+		============================================ -->       
+        <script src="js/owl.carousel.min.js"></script>
+		
+		<!-- jquery scrollUp js 
+		============================================ -->
+        <script src="js/jquery.scrollUp.min.js"></script>
+		
+		<!-- price-slider js
+		============================================ --> 
+        <script src="js/price-slider.js"></script>
+		
+		<!-- elevateZoom js 
+		============================================ -->
+		<script src="js/jquery.elevateZoom-3.0.8.min.js"></script>
+		
+		<!-- jquery.bxslider.min.js
+		============================================ -->       
+        <script src="js/jquery.bxslider.min.js"></script>
+		
+		<!-- mobile menu js
+		============================================ -->  
+		<script src="js/jquery.meanmenu.js"></script>
+        
+   		<!-- wow js
+		============================================ -->       
+        <script src="js/wow.js"></script>
+        
+   		<!-- plugins js
+		============================================ -->         
+        <script src="js/plugins.js"></script>
+        
+   		<!-- main js
+		============================================ -->           
+        <script src="js/main.js"></script>
+    </body>
+
+<!-- Mirrored from htmldemo.net/lavoro/lavoro/index-5.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 30 Jan 2024 07:29:40 GMT -->
+</html>
