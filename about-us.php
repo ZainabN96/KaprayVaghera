e5497da1302e364fd5392b6f69468198a10e2bd3
Sanashahid <sanashd20@gmail.com>
2024-02-15 11:41:05 +0500

    about us added

--- about-us.php
+++ about-us.php
@@ -31,18 +31,19 @@
 						</div>
 					</div>
 				</div>
-				<div class="row">
+				
+				<!-- <div class="row">
 					<div class="col-lg-7 col-12">
 						<div class="about-page-cntent">
 							<h3>The standard lorem ipsum passage</h3>
-							<!-- <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?</p> -->
+							<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?</p>
 							<blockquote>
 								<p>Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur</p>
 								<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In, non! Quas, voluptatum maiores facere blanditiis fuga debitis eius eum necessitatibus animi recusandae reprehenderit velit!
 								</p>
 								<p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id blanditiis nesciunt corporis labore non culpa eligendi eius. Consequuntur commodi porro atque corporis, esse tenetur quidem quam cum in provident delectus iusto! Necessitatibus, earum voluptate laudantium architecto facere sit inventore debitis.</p>
 							</blockquote>
-							<!-- <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eu nisi ac mi malesuada vestibulum. Phasellus tempor nunc eleifend cursus molestie. Mauris lectus arcu, pellentesque at sodales sit amet, condimentum id nunc. Donec ornare mattis suscipit. Praesent fermentum accumsan vulputate.</p> -->
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eu nisi ac mi malesuada vestibulum. Phasellus tempor nunc eleifend cursus molestie. Mauris lectus arcu, pellentesque at sodales sit amet, condimentum id nunc. Donec ornare mattis suscipit. Praesent fermentum accumsan vulputate.</p>
 						</div>
 					</div>
 					<div class="col-lg-5 col-12">
@@ -50,12 +51,37 @@
 							<img width="650" height="538" src="img/about/image-650x538.jpg" alt="banner1">
 						</div>
 					</div>
-				</div>
+				</div> -->
+
+<div class="about-container">
+        <div class="left">
+            <h1 class="font_4 wixui-rich-text__text" style="text-align:center; line-height:1.25em; font-size:72px;"><span style="font-weight:bold;" class="wixui-rich-text__text"><span style="text-transform:uppercase;" class="wixui-rich-text__text">KAPRAY VAPRAY </span></span></span></h1>
+           
+            <hr>
+            <h3 class="font_7 wixui-rich-text__text" style="line-height:1.67em; text-align:center; font-size:20px;"><span class="color_15 wixui-rich-text__text">Wrap yourself in elegance with our hand-embroidered kurtas, where tradition meets contemporary style.</span></h3>
+       
+        </div>
+        <div class="right"></div>
+    </div>
+
+    <div class="scroll-container">
+        <div class="left-scroll"></div>
+        <div class="right-scroll">
+            <p>Kapray Vaghera stands out in the fashion industry not only as a brand but as a symbol of tradition, craftsmanship, and sustainability. Founded in 2001 by Ayesha Chaudhury, a graduate of PSFD, the brand has been on a mission to revive old Pakistani crafts while creating timeless pieces that transcend trends.
+One of Kapray Vaghera's core principles is to offer customers the ability to curate their own unique style by mixing and matching different pieces, from salwars to pants to stoles. The brand places a strong emphasis on the quality of the cloth, ensuring that every garment is made to the highest standards.
+What truly sets Kapray Vaghera apart is its commitment to empowering women through employment opportunities. </p>
+            <p>By employing women to hand embroider garments from their homes, the brand not only supports their livelihoods but also enables them to maintain a balance between work and home life.
+Through carefully crafted patterns perfected over the years, Kapray Vaghera produces one-of-a-kind pieces that never go out of style. This approach not only contributes to sustainable fashion but also preserves and celebrates Pakistan's rich textile heritage.
+In essence, Kapray Vaghera is more than just a fashion brand; it's a symbol of empowerment, craftsmanship, and timeless elegance.</p>
+          
+        </div>
+    </div>
+
 			</div>
 		</div>
 		<!-- hello about end -->
 		<!-- service about start -->
-		<div class="our-services-info">
+		<!-- <div class="our-services-info">
 			<div class="container">
 				<div class="row">
 					<div class="col-12">
@@ -63,8 +89,9 @@
 							<h3 class="title text-headss">Our services</h3>
 						</div>
 					</div>
-				</div><!-- End .row -->
-				<div class="row text-center">
+				</div> -->
+				<!-- End .row -->
+				<!-- <div class="row text-center">
 					<div class="our-services-inner">
 						<div class="row">
 							<div class="col-md-4">
@@ -72,23 +99,29 @@
 									<p><i class="fa fa-star"></i></p>
 									<h4>Branding</h4>
 									<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
-								</div><!-- End .single-service -->
-							</div><!-- End .col-md-4 -->
-							<div class="col-md-4">
+								</div> -->
+								<!-- End .single-service -->
+							<!-- </div> -->
+							<!-- End .col-md-4 -->
+							<!-- <div class="col-md-4">
 								<div class="single-service">
 									<p><i class="fa fa-heart"></i></p>
 									<h4>Awesome Design</h4>
 									<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
-								</div><!-- End .single-service -->
-							</div><!-- End .col-md-4 -->
-							<div class="col-md-4">
+								</div> -->
+								<!-- End .single-service -->
+							<!-- </div> -->
+							<!-- End .col-md-4 -->
+							<!-- <div class="col-md-4">
 								<div class="single-service">
 									<p><i class="fa fa-camera-retro"></i></p>
 									<h4>Photography</h4>
 									<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
-								</div><!-- End .single-service -->
-							</div><!-- End .col-md-4 -->
-						</div>
+								</div> -->
+								<!-- End .single-service -->
+							<!-- </div> -->
+							<!-- End .col-md-4 -->
+						<!-- </div>
 					</div>
 					<div class="clearfix"></div>
 					<div class="our-services-inner">
@@ -98,27 +131,36 @@
 									<p><i class="fa fa-cog"></i></p>
 									<h4>Reponsive</h4>
 									<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
-								</div><!-- End .single-service -->
-							</div><!-- End .col-md-4 -->
-							<div class="col-md-4">
+								</div> -->
+								<!-- End .single-service -->
+							<!-- </div> -->
+							<!-- End .col-md-4 -->
+							<!-- <div class="col-md-4">
 								<div class="single-service">
 									<p><i class="fa fa-eye"></i></p>
 									<h4>Retina ready</h4>
 									<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
-								</div><!-- End .single-service -->
-							</div><!-- End .col-md-4 -->
-							<div class="col-md-4">
+								</div>
+								
+							</div> -->
+							<!-- End .col-md-4 -->
+							<!-- <div class="col-md-4">
 								<div class="single-service">
 									<p><i class="fa fa-comments-o"></i></p>
 									<h4>Online Support</h4>
 									<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
-								</div><!-- End .single-service -->
-							</div><!-- End .col-md-4 -->
-						</div>
-					</div><!-- End .our-services-inner -->
-				</div><!-- End .row -->
-			</div><!-- End .container -->
-		</div>
+								</div> -->
+								<!-- End .single-service -->
+							<!-- </div> -->
+							<!-- End .col-md-4 -->
+						<!-- </div>
+					</div>
+					
+				</div>
+				
+			</div> -->
+			<!-- End .container -->
+		<!-- </div> -->
 		<!-- service about end -->
 		<!-- meet about start -->
 		<!-- <div class="home-our-team">
