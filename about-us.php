77e8db4b8f98cddad44fbb19ffd7f8c33983a957
Sanashahid <sanashd20@gmail.com>
2024-02-19 16:17:21 +0500

    about us formated

--- about-us.php
+++ about-us.php
@@ -1,47 +1,57 @@
-<?php 
-	include 'include/header2.php'; 
+<?php
+include 'include/header2.php';
 ?>
-		
 
-		<!-- hello about start -->
+
+<!-- hello about start -->
 <div class="home-hello-info pt-5">
 	<div class="container mt-5">
 
-    <div class="fade-heading" data-aos="fade-up">
-      <p><span class="part"> Kapray Vaghera </span> stands out in the fashion industry not only as a brand but as a symbol of tradition, craftsmanship, and sustainability. </p>
-    </div>
-	<div class="scroll-container"  data-aos="fade-up" data-aos-delay="400" >
-			<div class="left" style="line-height:1.67em; text-align:center; font-size:18px;">			
+		<div class="fade-heading" data-aos="fade-up">
+			<p><span class="part"> Kapray Vaghera </span> stands out in the fashion industry not only as a brand but as
+				a symbol of tradition, craftsmanship, and sustainability. </p>
+		</div>
+		<div class="scroll-container" data-aos="fade-up" data-aos-delay="400">
+			<div class="left" style="line-height:1.67em; text-align:center; font-size:18px;">
 
-					<p>Founded in 2001 by Ayesha Chaudhury, a graduate of PSFD, the brand has been on a mission to revive old Pakistani crafts while creating timeless pieces that transcend trends.
-                    One of Kapray Vaghera's core principles is to offer customers the ability to curate their own unique style by mixing and matching different pieces, from salwars to pants to stoles. The brand places a strong emphasis on the quality of the cloth, ensuring that every garment is made to the highest standards.
-					What truly sets Kapray Vaghera apart is its commitment to empowering women through employment opportunities. 
-					</p>
-					</span>
+				<p>Founded in 2001 by Ayesha Chaudhury, a graduate of PSFD, the brand has been on a mission to revive
+					old Pakistani crafts while creating timeless pieces that transcend trends.
+					One of Kapray Vaghera's core principles is to offer customers the ability to curate their own unique
+					style by mixing and matching different pieces, from salwars to pants to stoles. The brand places a
+					strong emphasis on the quality of the cloth, ensuring that every garment is made to the highest
+					standards.
+					What truly sets Kapray Vaghera apart is its commitment to empowering women through employment
+					opportunities.
+				</p>
+				</span>
 				</h1>
-			
+
 			</div>
 
 			<div class="right"></div>
-    	</div>
+		</div>
 
-<div class="scroll-container"  data-aos="fade-up" data-aos-delay="600">
+		<div class="scroll-container" data-aos="fade-up" data-aos-delay="600">
 			<div class="left-scroll"></div>
-        	<div class="right-scroll" style="line-height:1.67em; text-align:center; font-size:18px;">
-            	<p>
-					By employing women to hand embroider garments from their homes, the brand not only supports their livelihoods but also enables them to maintain a balance between work and home life.
-					Through carefully crafted patterns perfected over the years, Kapray Vaghera produces one-of-a-kind pieces that never go out of style. This approach not only contributes to sustainable fashion but also preserves and celebrates Pakistan's rich textile heritage.
-					In essence, Kapray Vaghera is more than just a fashion brand; it's a symbol of empowerment, craftsmanship, and timeless elegance.
+			<div class="right-scroll" style="line-height:1.67em; text-align:center; font-size:18px;">
+				<p>
+					By employing women to hand embroider garments from their homes, the brand not only supports their
+					livelihoods but also enables them to maintain a balance between work and home life.
+					Through carefully crafted patterns perfected over the years, Kapray Vaghera produces one-of-a-kind
+					pieces that never go out of style. This approach not only contributes to sustainable fashion but
+					also preserves and celebrates Pakistan's rich textile heritage.
+					In essence, Kapray Vaghera is more than just a fashion brand; it's a symbol of empowerment,
+					craftsmanship, and timeless elegance.
 				</p>
-          
-        	</div>
-    	</div> 
+
+			</div>
+		</div>
 
 
 	</div>
 </div>
-		<!-- hello about end -->
+<!-- hello about end -->
 
-		<!-- FOOTER START -->
-		<?php include 'include/footer.php' ?>
-		<!-- FOOTER END -->
+<!-- FOOTER START -->
+<?php include 'include/footer.php' ?>
+<!-- FOOTER END -->
