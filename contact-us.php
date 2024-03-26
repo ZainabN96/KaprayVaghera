f7713de69be352a648c2f79efa3b518c3c3daa62
Sanashahid <sanashd20@gmail.com>
2024-03-26 13:40:12 +0500

    contactus page

--- contact-us.php
+++ contact-us.php
@@ -11,129 +11,313 @@
     <!-- header area start -->
     <?php include 'includes/navBar2.php' ?>
 		<!-- contact-details start -->
-		<main class="main">
-            <!-- <div class="page-header" style="background-image: url(images/page-header.jpg)">
-                <h1 class="page-title">Get in touch</h1>
-            </div> -->
-            <div class="page-content mt-10 pt-10">
-                <section class="contact-section mt-5 pt-5">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-lg-3 col-md-4 col-xs-5 ls-m pt-3">
-                                <h2 class="font-weight-bold text-uppercase ls-m mb-2">Contact us</h2>
-                                <!-- <p>Looking for help? Fill the form and start a new adventure.</p> -->
-
-                                <h4 class="mb-1 text-uppercase">Headquarters</h4>
-                                <p>25 Main Market Rd, Gulberg 2, Lahore, Punjab</p>
-
-                                <h4 class="mb-1 text-uppercase">Phone</h4>
-                                <p><a href="tel:#">0123456789</a></p>
-
-                                <h4 class="mb-1 text-uppercase">Support</h4>
-                                <p>
-                                    <a href="#">support@your-domain.com</a><br>
-                                    <a href="#">help@your-domain.com</a><br>
-                                  
-                                </p>
-                            </div>
-                            <div class="col-lg-9 col-md-8 col-xs-7">
-                                <form class="ml-lg-2 pt-8 pb-10 pl-4 pr-4 pl-lg-6 pr-lg-6 grey-section" action="#">
-                                    <h3 class="ls-m mb-1">Let’s Connect</h3>
-                                    <p class="text-grey">Your email addres will not be published. Required fields are
-                                        marked *</p>
-                                    <div class="row">
-                                        <div class="col-md-6 mb-4">
-                                            <input class="form-control" type="text" placeholder="Name *" required>
-                                        </div>
-                                        <div class="col-md-6 mb-4">
-                                            <input class="form-control" type="email" placeholder="Email *" required>
-                                        </div>
-                                        <div class="col-12 mb-4">
-                                            <textarea class="form-control" required
-                                                placeholder="Your Message *"></textarea>
-                                        </div>
-                                    </div>
-                                    <button class="btn btn-md btn-primary mb-2">Send Message</button>
-                                </form>
+	
+        <div class="ht__bradcaump__area">
+            <div class="ht__bradcaump__wrap" style="height: 150px;">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <div class="bradcaump__inner">
+                                <!-- <nav class="bradcaump-inner">
+                                  <a class="breadcrumb-item" href="index.php">Home</a>
+                                  <span class="brd-separetor"><i class="zmdi zmdi-chevron-right"></i></span>
+                                  <span class="breadcrumb-item active">Contact Us</span>
+                                </nav> -->
                             </div>
                         </div>
                     </div>
-                </section>
-                <!-- End About Section-->
-
-                <!-- <section class="store-section pt-10 pb-10">
-                    <div class="container">
-                        <h2 class="title mt-2">Our Store</h2>
-                        <div class="row cols-sm-12 cols-lg-12">
-                            <div class="store">
-                                <figure>
-                                    <img src="img/kpray stor.jpg" alt="store" width="180" height="140">
-                                    <h4 class="overlay-visible">Lahore</h4>
-                                    <div class="overlay overlay-transparent">
-                                        <a class="mt-8" href="mail:#">mail@newyorkdonaldstore.com</a>
-                                        <a href="tel:#">Phone: (123) 456-7890</a>
-                                        <div class="social-links mt-1">
-                                            <a href="#" class="social-link social-facebook fa fa-facebook-f"></a>
-                                            <a href="#" class="social-link social-twitter fa fa-twitter"></a>
-                                            <a href="#" class="social-link social-linkedin fa fa-linkedin"></a>
-                                        </div>
-                                    </div>
-                                </figure>
-                            </div> -->
-                            <!-- <div class="store">
-                                <figure>
-                                    <img src="images/subpages/store-2.jpg" alt="store" width="280" height="280">
-                                    <h4 class="overlay-visible">London</h4>
-                                    <div class="overlay overlay-transparent">
-                                        <a class="mt-8" href="mail:#">mail@londondonaldstore.com</a>
-                                        <a href="tel:#">Phone: (123) 456-7890</a>
-                                        <div class="social-links mt-1">
-                                            <a href="#" class="social-link social-facebook fab fa-facebook-f"></a>
-                                            <a href="#" class="social-link social-twitter fab fa-twitter"></a>
-                                            <a href="#" class="social-link social-linkedin fab fa-linkedin-in"></a>
-                                        </div>
-                                    </div>
-                                </figure>
-                            </div> -->
-                            <!-- <div class="store">
-                                <figure>
-                                    <img src="images/subpages/store-3.jpg" alt="store" width="280" height="280">
-                                    <h4 class="overlay-visible">Oslo</h4>
-                                    <div class="overlay overlay-transparent">
-                                        <a class="mt-8" href="mail:#">mail@oslodonaldstore.com</a>
-                                        <a href="tel:#">Phone: (123) 456-7890</a>
-                                        <div class="social-links mt-1">
-                                            <a href="#" class="social-link social-facebook fab fa-facebook-f"></a>
-                                            <a href="#" class="social-link social-twitter fab fa-twitter"></a>
-                                            <a href="#" class="social-link social-linkedin fab fa-linkedin-in"></a>
-                                        </div>
-                                    </div>
-                                </figure>
-                            </div> -->
-                            <!-- <div class="store">
-                                <figure>
-                                    <img src="images/subpages/store-4.jpg" alt="store" width="280" height="280">
-                                    <h4 class="overlay-visible">Stockholm</h4>
-                                    <div class="overlay overlay-transparent">
-                                        <a class="mt-8" href="mail:#">mail@stockholmdonaldstore.com</a>
-                                        <a href="tel:#">Phone: (123) 456-7890</a>
-                                        <div class="social-links mt-1">
-                                            <a href="#" class="social-link social-facebook fab fa-facebook-f"></a>
-                                            <a href="#" class="social-link social-twitter fab fa-twitter"></a>
-                                            <a href="#" class="social-link social-linkedin fab fa-linkedin-in"></a>
-                                        </div>
-                                    </div>
-                                </figure>
-                            </div> -->
-                        <!-- </div>
-                    </div>
-                </section> -->
-                <!-- End Store Section -->
-
-              
+                </div>
             </div>
-        </main>
-        <!-- End Main -->
+        </div>
+        <!-- End Bradcaump area -->
+        <!-- Start Contact Area -->
+        <section class="htc__contact__area ptb--100 bg__white">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-7 col-md-6 col-sm-12 col-xs-12">
+                        <div class="map-contacts--2">
+                            <div id="googleMap"></div>
+                        </div>
+                    </div>
+                    <div class="col-lg-5 col-md-6 col-sm-12 col-xs-12">
+                        <h2 class="title__line--6">CONTACT US</h2>
+                        <div class="address">
+                            <div class="address__icon">
+                            <i class="bi bi-geo-alt"></i>
+                            </div>
+                            <div class="address__details">
+                                <h2 class="ct__title">our address</h2>
+                                <p>Main Market Rd, Gulberg 2, Lahore, Punjab </p>
+                            </div>
+                        </div>
+
+                      
+                        <div class="address">
+                            <div class="address__icon">
+                            <i class="bi bi-clock"></i>
+                            </div>
+                            <div class="address__details">
+                                <h2 class="ct__title">opening hour</h2>
+                                <p> Mon - Sun : 12:00 pm - 10:00 pm</p>
+                            </div>
+                        </div>
+
+                        <div class="address">
+                            <div class="address__icon">
+                            <i class="bi bi-telephone"></i>
+                            </div>
+                            <div class="address__details">
+                                <h2 class="ct__title">Phone Number</h2>
+                                <p>123-6586-587456</p>
+                            </div>
+                        </div>
+                    </div>      
+                </div>
+                <div class="row">
+                    <div class="contact-form-wrap mt--60">
+                        <div class="col-xs-12">
+                            <div class="contact-title">
+                                <h2 class="title__line--6">SEND A MAIL</h2>
+                            </div>
+                        </div>
+                        <div class="col-xs-12">
+                            <form id="contact-form" action="#" method="post">
+                                <div class="single-contact-form">
+                                    <div class="contact-box name">
+                                        <input type="text" id="name" name="name" placeholder="Your Name*">
+                                        <input type="email" id="email" name="email" placeholder="Email*">
+										<input type="email" id="mobile" name="mobile" placeholder="Mobile*">
+                                    </div>
+                                </div>
+                                
+                                <div class="single-contact-form">
+                                    <div class="contact-box message">
+                                        <textarea name="message" id="message" placeholder="Your Message"></textarea>
+                                    </div>
+                                </div>
+                                <div class="contact-btn">
+                                    <button type="button" onclick="send_message()" class="fv-btn">Send MESSAGE</button>
+                                </div>
+                            </form>
+                            <div class="form-output">
+                                <p class="form-messege"></p>
+                            </div>
+                        </div>
+                    </div> 
+                </div>
+            </div>
+        </section>
+        <!-- End Contact Area -->
+		<!-- Google Map js -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBmGmeot5jcjdaJTvfCmQPfzeoG_pABeWo "></script>
+    <script src="js/contact-map.js"></script>
+    <script>
+        // When the window has finished loading create our google map below
+        google.maps.event.addDomListener(window, 'load', init);
+
+        function init() {
+            // Basic options for a simple Google Map
+            // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+            var mapOptions = {
+                // How zoomed in you want the map to start at (always required)
+                zoom: 12,
+
+                scrollwheel: false,
+
+                // The latitude and longitude to center the map (always required)
+                center: new google.maps.LatLng(23.7286, 90.3854), // New York
+
+                // How you would like to style the map. 
+                // This is where you would paste any style found on Snazzy Maps.
+                 styles: 
+        [ {
+                "featureType": "all",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "saturation": 36
+                    },
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 40
+                    }
+                ]
+            },
+            {
+                "featureType": "all",
+                "elementType": "labels.text.stroke",
+                "stylers": [
+                    {
+                        "visibility": "on"
+                    },
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 16
+                    }
+                ]
+            },
+            {
+                "featureType": "all",
+                "elementType": "labels.icon",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 20
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 17
+                    },
+                    {
+                        "weight": 1.2
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 20
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 21
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 17
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 29
+                    },
+                    {
+                        "weight": 0.2
+                    }
+                ]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 18
+                    }
+                ]
+            },
+            {
+                "featureType": "road.local",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 16
+                    }
+                ]
+            },
+            {
+                "featureType": "transit",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 19
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#141516"
+                    },
+                    {
+                        "lightness": 17
+                    }
+                ]
+            }
+        ]
+            };
+
+            // Get the HTML DOM element that will contain your map 
+            // We are using a div with id="map" seen below in the <body>
+            var mapElement = document.getElementById('googleMap');
+
+            // Create the Google Map using our element and options defined above
+            var map = new google.maps.Map(mapElement, mapOptions);
+
+            // Let's also add a marker while we're at it
+            var marker = new google.maps.Marker({
+                position: new google.maps.LatLng(23.7286, 90.3854),
+                map: map,
+                title: 'Ramble!',
+                icon: 'images/icons/map-2.png',
+                animation:google.maps.Animation.BOUNCE
+
+            });
+        }
+    </script>
+
+
 	  <!-- FOOTER START -->
 	  <?php include 'includes/footer.php'; ?>
     <!-- FOOTER END -->
