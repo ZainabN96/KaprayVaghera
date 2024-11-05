4a0ce160c7b06e71ddb47d13f14b15f4df5fd5f8
zainabn096 <zainabnaveed2512@gmail.com>
2024-11-05 10:40:26 +0500

    product details

--- admin/product-detail.php
+++ admin/product-detail.php
@@ -2,11 +2,22 @@
 require('top.inc.php');
 ?>
 
-<Section>
-	<div>
-		<h1>Page</h1>
+<div class="content pb-0">
+	<div class="product">
+	   <div class="row">
+		  	<div class="col-xl-12">
+				<div class="card">
+					<div class="card-body">
+						<h4 class="box-title">Product Details </h4>
+					</div>
+					<div class="card-body card-block">
+						<H1></h1>
+					</div>
+				</div>
+		  </div>
+	   </div>
 	</div>
-</Section>
+</div>
 
 <?php
 require('footer.inc.php');
